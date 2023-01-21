1148c7cf32792c907c9013580c4e752681b6c20a
Diana Karpeev <2059788@johnabbottcollege.net>
2023-01-21 18:09:09 -0500

    readded index content

--- src/components/BubbleDetail.tsx
+++ src/components/BubbleDetail.tsx
@@ -1,7 +1,7 @@
 import "./bubble.module.scss"
 
 export default function BubbleDetail(){
-	return(<body>
+	return(
 	  <section id="postContainer">
 		<header>
 		  <div id="poster">
@@ -14,6 +14,5 @@ export default function BubbleDetail(){
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
 		  </div>
 		</main>
-	  </section>
-	</body>)
+	  </section>)
   }
