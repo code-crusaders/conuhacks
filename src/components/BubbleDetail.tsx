3265ebb6d3fec6310c2658cd3fb15433c85e4904
Daniel Thorp <arcanistzed@gmail.com>
2023-01-21 18:11:50 -0500

    style: format code
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/BubbleDetail.tsx
+++ src/components/BubbleDetail.tsx
@@ -1,19 +1,32 @@
-import "./bubble.module.scss"
+import "./bubble.module.scss";
 
-export default function BubbleDetail(){
-	return(<body>
-	  <section id="postContainer">
-		<header>
-		  <div id="poster">
-			<img id="icon" src="/imgs/favicon.ico" width="40px" height="40px"></img>
-			<h3 id="username">username</h3>
-		  </div>
-		</header>
-		<main>
-		  <div id="textbox">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-		  </div>
-		</main>
-	  </section>
-	</body>)
-  }
+export default function BubbleDetail() {
+	return (
+		<section id="postContainer">
+			<header>
+				<div id="poster">
+					<img id="icon" src="/imgs/favicon.ico" width="40px" height="40px"></img>
+					<h3 id="username">username</h3>
+				</div>
+			</header>
+			<main>
+				<div id="textbox">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor
+						sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+						nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+						dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+						sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+						laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+						incididunt ut labore.
+					</p>
+				</div>
+			</main>
+		</section>
+	);
+}
